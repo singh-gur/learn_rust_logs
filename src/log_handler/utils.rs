02f299b6189ca1617fec77c5351ac5589f9c0ab3
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2025-07-25 12:53:07 -0400

    Refactor log extraction function: rename to extract_log_type and update to filter by log type

--- src/log_handler/utils.rs
+++ src/log_handler/utils.rs
@@ -1,13 +1,13 @@
 use std::io::Error;
 
-pub fn extract_errors_from_logs(logs: &String) -> Vec<String> {
-    let mut errors = vec![];
+pub fn extract_log_type(logs: &String, log_type: &str) -> Vec<String> {
+    let mut filtered_logs = vec![];
     for line in logs.lines() {
-        if line.contains("ERROR") {
-            errors.push(line.to_string());
+        if line.contains(log_type) {
+            filtered_logs.push(line.to_string());
         }
     }
-    errors
+    filtered_logs
 }
 
 pub fn divide(a: i32, b: i32) -> Result<i32, Error> {
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::fs::read_to_string;
 
 mod log_handler;
-use log_handler::utils::{divide, extract_errors_from_logs};
+use log_handler::utils::{divide, extract_log_type};
 
 fn main() {
     match read_to_string("data/logs.txt") {
-        Ok(data) => println!("Errors:\n{:#?}", extract_errors_from_logs(&data)),
+        Ok(data) => println!("Warnings:\n{:#?}", extract_log_type(&data, "WARNING")),
         Err(e) => eprintln!("Error reading file: {}", e),
     }
     match divide(4, 2) {
