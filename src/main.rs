e0f84dbd96311bb9c1e6f3ca97f52559b57d6d6a
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2025-08-03 16:19:13 -0400

    more changes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::fs::{read_to_string, write};
-
+use std::io::Error;
 mod log_handler;
 use log_handler::utils::extract_log_type;
 
-fn main() {
-    let text = read_to_string("data/run.log").expect("Failed to read logs");
+fn main() -> Result<(), Error> {
+    let text = read_to_string("data/run.log")?;
 
     let error_logs = extract_log_type(text.as_str(), "ERROR");
-    write("data/errors.log", error_logs.join("\n")).expect("Unable to write error logs");
+    write("data2/errors.log", error_logs.join("\n"))?;
+
+    Ok(())
 }
